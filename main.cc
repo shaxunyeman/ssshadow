e23b5e0607422c10428561a8348537b4785bb66f
db.liu <shaxunyeman@gmail.com>
2018-07-26 10:43:45 +0800

    implement send function
    Summary: this release version will implement communicate.
    
    Test Plan: using raw socket implement send function.
    
    Reviewers: devpeersafe
    
    Reviewed By: devpeersafe
    
    Differential Revision: http://cr.peersafe.com/D13

--- main.cc
+++ main.cc
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int send(int socket, const char* data, size_t size) {
+  cout 
+    << "send " << string(data, size) 
+    << " to " << socket
+    << endl;
+  return size;
+}
 
 int main(int argc, char** argv) {
   cout << "hello peersafe." << endl;
