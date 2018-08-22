53b008920454f2a62dbfae8732bce70f232079b4
db.liu <shaxunyeman@gmail.com>
2018-08-22 18:43:36 +0800

    support travis

--- helper.h
+++ helper.h
@@ -4,14 +4,14 @@
 void log0(const std::string& data) {
   std::cout
     << ": " << data
-    << endl;
+    << std::endl;
 }
 
 void log1(const std::string& data, const std::string& title) {
   std::cout
     << title << ": "
     << data
-    << endl;
+    << std::endl;
 }
 
 int event(const std::string& data) {
--- main.cc
+++ main.cc
@@ -5,72 +5,7 @@
 
 using namespace std;
 
-int send(int socket, const char* data, size_t size) {
-  cout 
-    << "send " << string(data, size) 
-    << " to " << socket
-    << endl;
-  return size;
-}
-
-int recv(int socket, char* buffer, size_t size) {
-  cout
-    << "recv message from " << socket
-    << endl;
-  return size;
-}
-
-int send_file(int socket, const char* filepath) {
-  cout
-    << "send a file names " << filepath
-    << " to " << socket
-    << endl;
-  return 0;
-}
-
-int recv_file(char* buffer, size_t size) {
-  cout
-    << "recv file data"
-    << endl;
-  return size;
-}
-
-bool verify(const user& user) {
-  cout
-    << "implement verify function."
-    << endl;
-
-  if (match(user.name()) == false) {
-    cout << "match name failure."
-    return false;
-  }
-
-  if (match(user.passwd()) == false) {
-    cout << "match passwd failure."
-    return false;
-  }
-  return true;
-}
-
 int main(int argc, char** argv) {
-  log0("start ... ");
-  int dst = 1000;
-  const char* msg = "hello,peerafe.";
-  send(dst, msg, strlen(msg)); 
-  event("send a msg ");
-
-  char buffer[1024] = {0};
-  recv(dst, buffer, 1024);
-  event("recv a msg ");
-
-  user u = {"peersafe", "pwd000"};
-  bool bok = verify(u);
-
-  cout << "verify: " << u << endl;
-
-  bok = send_file(dst, "/usr/test");
-  cout << "send result: " << bok << endl;
-  event("send a file");
-
+  std::cout << "hi, sshadow..." << std::endl;
   return 0;
 }
